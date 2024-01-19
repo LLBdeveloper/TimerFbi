2971778ec6314bded3e125d0dfdd42a35a720a17
Lautaro Bertera <bertera.lautaro@gmail.com>
2024-01-19 01:06:34 -0300

    Timer FBI -beta

--- src/components/Bip.jsx
+++ src/components/Bip.jsx
@@ -37,16 +37,20 @@ function Bip() {
         setTiempoSeg(0)
         iniciar()
         setTimeout(contador, 4000)
+        setTimeout(()=>{
+            alert("GRACIAS POR USAR NUESTRO TIMER PARA FBI, puede volver a usarlo sin recargar la app.")
+        }, 9000)
     }
 
     return (
         <> 
-            <h1> - Timer para FBI - </h1>
+            <h1 style={{fontSize:'5rem'}} > - Timer para FBI - </h1>
+            <p style={{color: 'red', fontSize: '2rem'}}>¿¿¿ ESTAN LISTOS TIRADORES ???</p>
+            <p  >Luego de dar click a INICIAR, en pocos segundos comienza el contador.  </p>
             <h2>Tiempo</h2>
-
-            <h3>{tiempoSeg}:{tiempoSeg >= 4 ? '0' : tiempoMil}</h3>
-            <button onClick={click} > sonar </button>
-            <p>Despues del click en sonar en 4 segundos inicia. <br></br><br></br> YOU ARE READY ???</p>
+            <h3 style={{fontSize:'3rem', color: 'green'}}>{tiempoSeg}:{tiempoSeg >= 4 ? '0' : tiempoMil}</h3>
+            <button onClick={click} style={{padding:'2rem 6rem 2rem 6rem', fontSize: '2rem'}}> INICIAR  </button>
+            
         </>
     )
 }
