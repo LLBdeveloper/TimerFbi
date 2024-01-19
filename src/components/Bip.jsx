4bb5938bfd76a085ad141917943d9fcbe1bfb72a
Lautaro Bertera <bertera.lautaro@gmail.com>
2024-01-19 00:14:27 -0300

    logica del TIMER FBI finalizado,falta estilos

--- src/components/Bip.jsx
+++ src/components/Bip.jsx
@@ -1,19 +1,51 @@
 import useSound from 'use-sound';
 import BipSound from '/src/assets/sound/BipSound.mp3';
+import { useState } from 'react';
+
 
 
 function Bip() {
 
-const [playSound] = useSound(BipSound)
-const iniciar = () => {
-    setTimeout(playSound, 4000)
-    setTimeout(playSound, 8000)
-}
+    const [tiempoSeg, setTiempoSeg] = useState (null)
+    const [tiempoMil, setTiempoMil] = useState (null)
+    const [playSound] = useSound(BipSound)
+
+    const iniciar = () => {
+        setTimeout(playSound, 4000)
+        setTimeout(playSound, 8000)
+    }
+
+    const contador = () => {
+        const intervalSeg = setInterval(() => {
+            setTiempoSeg(prev => {
+                if (prev >= 4) {
+                    clearInterval(intervalSeg)
+                    clearInterval(intervalMil)
+                    return prev
+                }
+                return prev + 1
+            })
+        }, 1000)
+
+        const intervalMil = setInterval(() => {
+            setTiempoMil(prev => (prev + 1) % 100)
+        }, 10)
+    }
+
+    const click = () =>{
+        setTiempoMil(0)
+        setTiempoSeg(0)
+        iniciar()
+        setTimeout(contador, 4000)
+    }
 
     return (
-        <div> 
-            <button onClick={iniciar}> sonar </button>
-        </div>
+        <> 
+            <h1> - Timer para FBI - </h1>
+            <h2>Tiempo</h2>
+            <h3>{tiempoSeg}:{tiempoSeg >= 4 ? '0' : tiempoMil}</h3>
+            <button onClick={click} > sonar </button>
+        </>
     )
 }
 
